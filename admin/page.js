e1de9df8bbd530e9f773bab8bb70482f845283ce
fhardi95 <fhardi.said@gmail.com>
2026-04-22 00:46:39 +0100

    Delete admin directory

--- admin/page.js
+++
@@ -1,9 +0,0 @@
-export default function Admin(){
-  return (
-    <main>
-      <h1>SEO Dashboard</h1>
-      <p>Génération pages programmatic SEO</p>
-      <button>Generate 1000 pages</button>
-    </main>
-  );
-}
